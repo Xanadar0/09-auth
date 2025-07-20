a5fde83eb47177e5a3f7a56ccc05bd6f9a3fb7dd
Xanadar0 <61867465+Xanadar0@users.noreply.github.com>
2025-07-20 14:09:31 +0300

    Initial commit: Add NoteHub Next.js app

---
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -0,0 +1,27 @@
+import fetchNoteId from "@/lib/api";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import NotePreviewClient from "./NotePreview.client";
+
+interface NoteModalProps {
+  params: Promise<{ id: string }>;
+}
+
+export default async function NoteModal({ params }: NoteModalProps) {
+  const { id } = await params;
+  const noteId = +id;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["note", noteId],
+    queryFn: () => fetchNoteId(noteId),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotePreviewClient />
+    </HydrationBoundary>
+  );
+}
---
+++ notes/filter/[...slug]/page.tsx
@@ -0,0 +1,25 @@
+import { fetchNotes } from "@/lib/api";
+import NotesClient from "./Notes.client";
+
+interface NotesProps {
+  params: Promise<{ slug: string[] }>;
+}
+
+export const revalidate = 60;
+
+export default async function Notes({ params }: NotesProps) {
+  const initialQuery = "";
+  const initialPage = 1;
+  const { slug } = await params;
+  const tag = slug[0] === "all" ? undefined : slug[0];
+  const data = await fetchNotes(initialQuery, initialPage, tag);
+
+  return (
+    <NotesClient
+      initialQuery={initialQuery}
+      initialPage={initialPage}
+      initialTag={tag}
+      initialData={data}
+    />
+  );
+}
